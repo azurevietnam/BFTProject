0e985b0f6dc696119e439c1f04012eefe01d060d
QuynhNguyen <QuynhNguyen@10.22.169.176>
2016-12-03 23:02:25 +0700

    edit FlightContext

--- src/java/com/entities/Flight.java
+++ src/java/com/entities/Flight.java
@@ -14,92 +14,91 @@ import java.sql.Date;
  */
 public class Flight implements Serializable {
 
-   private String flightID, flightName, airlineName, fromLocation, toLocation;
-   private Date departureTime, arrivalTime;
-   private int firstClassSeats, businessSeats, economySeats;
+    private String flightID, flightName, airlineName, fromLocation, toLocation;
+    private String departureTime, arrivalTime;
+    private int firstClassSeats, businessSeats, economySeats;
 
-   public Flight() {
-   }
+    public Flight() {
+    }
 
-   public String getFlightID() {
-      return flightID;
-   }
+    public String getFlightID() {
+        return flightID;
+    }
 
-   public void setFlightID(String flightID) {
-      this.flightID = flightID;
-   }
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
 
-   public String getFlightName() {
-      return flightName;
-   }
+    public String getFlightName() {
+        return flightName;
+    }
 
-   public void setFlightName(String flightName) {
-      this.flightName = flightName;
-   }
+    public void setFlightName(String flightName) {
+        this.flightName = flightName;
+    }
 
-   public String getAirlineName() {
-      return airlineName;
-   }
+    public String getAirlineName() {
+        return airlineName;
+    }
 
-   public void setAirlineName(String airlineName) {
-      this.airlineName = airlineName;
-   }
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
 
-   public String getFromLocation() {
-      return fromLocation;
-   }
+    public String getFromLocation() {
+        return fromLocation;
+    }
 
-   public void setFromLocation(String fromLocation) {
-      this.fromLocation = fromLocation;
-   }
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
 
-   public String getToLocation() {
-      return toLocation;
-   }
+    public String getToLocation() {
+        return toLocation;
+    }
 
-   public void setToLocation(String toLocation) {
-      this.toLocation = toLocation;
-   }
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
 
-   public Date getDepartureTime() {
-      return departureTime;
-   }
+    public String getDepartureTime() {
+        return departureTime;
+    }
 
-   public void setDepartureTime(Date departureTime) {
-      this.departureTime = departureTime;
-   }
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 
-   public Date getArrivalTime() {
-      return arrivalTime;
-   }
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
 
-   public void setArrivalTime(Date arrivalTime) {
-      this.arrivalTime = arrivalTime;
-   }
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 
-   public int getFirstClassSeats() {
-      return firstClassSeats;
-   }
+    public int getFirstClassSeats() {
+        return firstClassSeats;
+    }
 
-   public void setFirstClassSeats(int firstClassSeats) {
-      this.firstClassSeats = firstClassSeats;
-   }
+    public void setFirstClassSeats(int firstClassSeats) {
+        this.firstClassSeats = firstClassSeats;
+    }
 
-   public int getBusinessSeats() {
-      return businessSeats;
-   }
+    public int getBusinessSeats() {
+        return businessSeats;
+    }
 
-   public void setBusinessSeats(int businessSeats) {
-      this.businessSeats = businessSeats;
-   }
+    public void setBusinessSeats(int businessSeats) {
+        this.businessSeats = businessSeats;
+    }
 
-   public int getEconomySeats() {
-      return economySeats;
-   }
+    public int getEconomySeats() {
+        return economySeats;
+    }
 
-   public void setEconomySeats(int economySeats) {
-      this.economySeats = economySeats;
-   }
-   
-   
+    public void setEconomySeats(int economySeats) {
+        this.economySeats = economySeats;
+    }
+
 }
